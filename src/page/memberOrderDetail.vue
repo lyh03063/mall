a2240b1c317ecb65c066a778d797c95e81d3931f
dai <846535173@qq.com>
2019-06-10 15:27:37 +0800

    戴广泽了修改

--- src/page/memberOrderDetail.vue
+++ src/page/memberOrderDetail.vue
@@ -1,12 +1,9 @@
 <template>
-  <div class="main">
-    订单详情
-  </div>
+  <div class="main">订单详情 戴广泽</div>
 </template>
 
 
 
 <style lang="scss" >
-@import "../assets/css/util.scss";//导入公共样式文件
-
+@import "../assets/css/util.scss"; //导入公共样式文件
 </style>
