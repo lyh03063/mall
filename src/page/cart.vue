9dac6e3b6960d37b054b687cc99de314112e7904
zhangjiawu <769535935@qq.com>
2019-06-11 22:51:18 +0800

    zjw

--- src/page/cart.vue
+++ src/page/cart.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="main">
-
-    购物车页面
-
-    翁大将军的购物车
-
+    <div style="  text-align: center;margin-top: 50px">
+      <div class="main-title">
+        <p style="color:#666">购物车块饿瘪了T.T</p>
+        <p style="font-size:14px;color:#999">快给我挑点宝贝</p>
+      </div>
+      <el-row>
+        <router-link to="./home">
+          <el-button style="margin-top:50px;color:red">去逛逛</el-button>
+        </router-link>
+      </el-row>
+    </div>
   </div>
 </template>
 
 
 
 <style lang="scss" >
-@import "../assets/css/util.scss";//导入公共样式文件
+@import "../assets/css/util.scss"; //导入公共样式文件
 
 </style>
